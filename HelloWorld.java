4e4f1b29362429426348017dcc3edd0d5b328e3b
qxx2631244011 <2631244011@qq.com>
2025-06-20 06:44:25 +0800

    分支B，HelloWorld中新增内容

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
         System.out.println("Hello, World!");
         Dog myDog = new Dog();
         myDog.name = "Buddy";
+        // 分支b新增内容
+        myDog.breed = "Golden Retriever";
     }
 }
